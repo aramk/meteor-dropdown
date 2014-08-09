b8ca892cc5ba2dc311775ef376f34a55cb4802ed
Aram Kocharyan <akarmenia@gmail.com>
2014-08-09 12:18:28 +1000

    Fixed AutoForm dependency.

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'meteor-dropdown',
+  name: 'dropdown',
   summary: 'A reactive dropdown widget for Meteor.'
 });
 
 Package.on_use(function(api) {
-  api.use(['templating', 'underscore', 'jquery', 'less'], 'client');
+  api.use(['templating', 'underscore', 'jquery'], 'client');
 
   api.add_files([
     'lib/dropdown.html',

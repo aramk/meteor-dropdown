41cbd2b213a68431975e81638a148fd6c75163e8
Aram Kocharyan <akarmenia@gmail.com>
2014-11-22 21:19:11 +1100

    Updated dropdown to support AutoForm v4.

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'aramk:dropdown',
   summary: 'A reactive dropdown widget',
-  version: '0.1.0',
+  version: '0.2.0',
   git: 'https://github.com/aramk/meteor-dropdown.git'
 });
 
 Package.on_use(function(api) {
   api.versionsFrom('METEOR@0.9.0');
   api.use(['templating', 'underscore', 'jquery'], 'client');
+  api.use(['aldeed:autoform@4.0.7'], 'client', {weak: true});
   api.add_files([
     'lib/dropdown.html',
     'lib/dropdown.js'

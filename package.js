a9d2085c68793fab99b1afb45a8a7e01a1e8cec5
Aram Kocharyan <akarmenia@gmail.com>
2014-11-28 17:19:35 +1100

    Fixed handling of the schemaKey attribute.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'aramk:dropdown',
   summary: 'A reactive dropdown widget',
-  version: '0.2.0',
+  version: '0.2.1',
   git: 'https://github.com/aramk/meteor-dropdown.git'
 });
 

6ef78a7873849ee87039008f722b8b744569950c
Aram Kocharyan <akarmenia@gmail.com>
2015-02-08 00:48:20 +1100

    Added support for Semantic-UI 1.8. Refactored logic for setting value and initialising to resolve bugs when used in apps.

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Package.on_use(function(api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.use(['templating', 'underscore', 'jquery'], 'client');
+  api.use(['templating', 'underscore', 'jquery', 'nooitaf:semantic-ui@1.8.1'], 'client');
   api.use(['aldeed:autoform@4.0.7'], 'client', {weak: true});
   api.add_files([
     'lib/dropdown.html',

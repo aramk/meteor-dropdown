2b5bcb93bc77ebdf5ebc05f666cd4eea819442ea
Aram Kocharyan <akarmenia@gmail.com>
2015-03-27 11:52:01 +1100

    Version update.

--- package.js
+++ package.js
@@ -1,14 +1,17 @@
 Package.describe({
   name: 'aramk:dropdown',
   summary: 'A reactive dropdown widget',
-  version: '0.4.1',
+  version: '0.4.2',
   git: 'https://github.com/aramk/meteor-dropdown.git'
 });
 
 Package.on_use(function(api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.use(['templating', 'underscore', 'jquery', 'semantic:ui-css'], 'client');
-  api.use(['aldeed:autoform@4.0.7'], 'client', {weak: true});
+  api.use(['templating', 'underscore', 'jquery'], 'client');
+  api.use([
+    'semantic:ui-css@1.11.4',
+    'aldeed:autoform@4.0.7'
+  ], 'client', {weak: true});
   api.add_files([
     'lib/dropdown.html',
     'lib/dropdown.js'

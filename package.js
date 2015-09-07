62b514d30aeec132c2bb44d2aa9b21c07c780285
Aram Kocharyan <akarmenia@gmail.com>
2015-09-07 16:16:49 +1000

    Updated semantic ui dependency.

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.on_use(function(api) {
   api.versionsFrom('METEOR@0.9.0');
   api.use(['templating', 'underscore', 'jquery'], 'client');
   api.use([
-    'semantic:ui-css@1.11.4',
+    'semantic:ui-css@2.0.8',
     'aldeed:autoform@5.1.2',
     'aramk:utility@0.8.7'
   ], 'client', {weak: true});
